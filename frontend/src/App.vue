7f46317a1190c9ccf5d2a2da3354ef06f1f57149
Roberto Rielo <roberto910907@gmail.com>
2023-03-02 00:26:51 -0500

    Added TailwindCSS configuration

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,16 +3,13 @@ import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
-  <header>
+  <header class="h-10 bg-sky-500 text-white">
     <div class="wrapper">
       <nav>
         <RouterLink to="/">Home</RouterLink>
       </nav>
     </div>
   </header>
+
   <RouterView />
 </template>
-
-<style scoped>
-
-</style>
